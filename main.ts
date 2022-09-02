1835c2ac7fea8b9efa985d87cf0f3f038ba92d61
oisin-gosson <72928509+oisin-gosson@users.noreply.github.com>
2022-09-02 09:15:00 +0100

    v2

--- main.ts
+++ main.ts
@@ -3,30 +3,28 @@ let Game = 0
 let Rounds = 0
 let Score = 0
 input.onGesture(Gesture.TiltLeft, function () {
-    Player = 2
+    Player = -2
 })
 input.onButtonPressed(Button.AB, function () {
-    Player = 3
+    Player = -3
 })
 input.onGesture(Gesture.Shake, function () {
     Game = randint(1, 3)
     Rounds += 1
 })
 input.onGesture(Gesture.TiltRight, function () {
-    Player = 1
+    Player = -1
 })
 basic.forever(function () {
-    if (Player < 2) {
-        Score += -1
+    if (Player + Game == 0) {
+        basic.showString("Draw")
     }
-    if (Player < 3) {
+    if (Player - Game == -2 || Player - Game == 1) {
+        basic.showString("Win")
         Score += 1
     }
-    if (Player < 3) {
-        Score += -1
-    }
-    if (Player < 4) {
-        Score += 1
+    if (Game - Player >= 3 && Game + Player <= 5) {
+        basic.showString("Lose")
     }
     if (Rounds == 3) {
         basic.showNumber(Score)
